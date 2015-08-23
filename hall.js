40ae1a39b7e8a50116d7ed8ddb36057c663f16f9
David <projecthalldev1@gmail.com>
2015-08-23 15:10:34 -0700

    Add game class and squareChecker method

--- hall.js
+++ hall.js
@@ -4,12 +4,13 @@
  * v1.0
  ********************************/
 
-// Make Square Object
-function Square() {
-	
+// Make Square Class
+function Square(x,y) {
+	this.x = x;
+	this.y = y;
 }
 
-// Make Board Object
+// Make Board Class
 function Board() {
 	// array of square objects
 	this.grid = [];
@@ -22,7 +23,26 @@ function Board() {
 
 		for (var j = 0; j < 16; j++) {
 			// second pass: columns
-			this.grid[i].push(new Square());
+			this.grid[i].push(new Square(i,j)); // Gives your square object x,y coordinates
 		}
 	}
+}
+
+function Game() {
+	// Method that checks for type of square (move or wall)
+	this.squareChecker = function(square) {
+			if (square.x % 2 === 0 && square.y % 2 === 0) {
+			console.log("This is a moveSquare"); // note, this is a great way to test stuff in JS
+		} else {
+			console.log("this is a wallSquare");
+		}
+	};
+	
+	// whose turn it is
+
+	// method verifies you're playing by the rules
+}
+
+function Player() {
+
 }
