979cafd5cb994b129700b5fd5e16e5ad80875916
David <projecthalldev1@gmail.com>
2015-08-23 12:35:44 -0700

    Add empty square object, add small board object

--- hall.js
+++ hall.js
@@ -1,1 +1,28 @@
-// test
+/*********************************
+ * Project Hall in JS
+ * Written by David & Liz
+ * v1.0
+ ********************************/
+
+// Make Square Object
+function Square() {
+	
+}
+
+// Make Board Object
+function Board() {
+	// array of square objects
+	this.grid = [];
+	// loop to add square objects to grid to make board
+
+	for (var i = 0; i < 3; i++)
+	{
+		// first pass: row
+		this.grid.push([]);
+
+		for (var j = 0; j < 3; j++) {
+			// second pass: columns
+			this.grid[i].push(new Square());
+		}
+	}
+}
